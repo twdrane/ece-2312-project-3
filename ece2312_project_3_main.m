5d7693ec0309927eb7b2341c50be6c372b20f518
Trevor Drane <twdrane@wpi.edu>
2023-03-02 13:53:04 -0500

    finis

--- ece2312_project_3_main.m
+++ ece2312_project_3_main.m
@@ -3,14 +3,60 @@ close all
 %isolateSpeech();
 %splitSignal();
 %downsampleSplit();
-downsampleSplitX2();
+%downsampleSplitX2();
+resythesize();
+
+function resythesize
+
+[audioHighpassTop,Fs] = audioread('teamG5-highpassdowntop.wav');
+[audioHighpassBottom,Fs] = audioread('teamG5-highpassdownbottom.wav');
+[audioLowpassTop,Fs] = audioread('teamG5-lowpassdowntop.wav');
+[audioLowpassBottom,Fs] = audioread('teamG5-lowpassdownbottom.wav');
+Fs
+
+upHighpassTop = upsample(audioHighpassTop,8);
+upHighpassBottom = upsample(audioHighpassBottom,8);
+upLowpassTop = upsample(audioLowpassTop,8);
+upLowpassBottom = upsample(audioLowpassBottom,8);
+Fs = Fs*8
+
+F = [0 4999/Fs 5000/Fs 1];
+A = [1 1 0 0];
+[fil1, fil2] = firls(255,F,A);
+upLowBottom = filter(fil1,fil2,upLowpassBottom);
+
+G = [0 4999/Fs 5000/Fs 9999/Fs 10000/Fs 1];
+B = [0 0 1 1 0 0];
+[fil3, fil4] = firls(256,G,B);
+upLowTop = filter(fil3,fil4,upLowpassTop);
+
+H = [0 9999/Fs 10000/Fs 14999/Fs 15000/Fs 1];
+C = [0 0 1 1 0 0];
+[fil3, fil4] = firls(256,H,C);
+upHighBottom = filter(fil3,fil4,upHighpassBottom);
+
+I = [0 14999/Fs 15000/Fs 1];
+D = [0 0 1 1];
+[fil3, fil4] = firls(256,I,D);
+upHighTop = filter(fil3,fil4,upHighpassTop);
+
+reconstructed = upLowBottom + upLowTop + upHighBottom + upHighTop;
+
+
+makeSpectrogram(reconstructed,Fs)
+%figure
+%makeSpectrogram(upLowTop,Fs);
+
+sound(reconstructed,Fs)
+
+end
 
 function downsampleSplitX2
 figure
 [audioHighpass,Fs] = audioread('teamG5-highpassdown1.wav');
 [audioLowpass,Fs] = audioread('teamG5-lowpassdown1.wav');
 
-F = [0 3999/Fs 4000/Fs 1];
+F = [0 4999/Fs 5000/Fs 1];
 A = [1 1 0 0];
 [fil1, fil2] = firls(255,F,A);
 lowpassBottomAudio = filter(fil1,fil2,audioLowpass);
@@ -19,7 +65,7 @@ B = [0 0 1 1];
 [fil3, fil4] = firls(256,F,B);
 lowpassTopAudio = filter(fil3,fil4,audioLowpass);
 
-F = [0 5999/Fs 6000/Fs 1];
+F = [0 4999/Fs 5000/Fs 1];
 A = [1 1 0 0];
 [fil1, fil2] = firls(255,F,A);
 highpassBottomAudio = filter(fil1,fil2,audioHighpass);
@@ -28,33 +74,45 @@ B = [0 0 1 1];
 [fil3, fil4] = firls(256,F,B);
 highpassTopAudio = filter(fil3,fil4,audioHighpass);
 
-subplot(2,1,1);
+subplot(4,1,3);
 makeSpectrogram(lowpassTopAudio,Fs)
 ylim([0 Fs/2])
-subplot(2,1,2);
+subplot(4,1,4);
 makeSpectrogram(lowpassBottomAudio,Fs)
 ylim([0 Fs/2])
-figure
-subplot(2,1,1);
+%figure
+subplot(4,1,1);
 makeSpectrogram(highpassTopAudio,Fs)
 ylim([0 Fs/2])
-subplot(2,1,2);
+subplot(4,1,2);
 makeSpectrogram(highpassBottomAudio,Fs)
 ylim([0 Fs/2])
 
-% downHighpass = downsample(audioHighpass,2);
-% downLowpass = downsample(audioLowpass,2);
-% Fs = Fs/2
-% figure
-% subplot(2,1,1);
-% makeSpectrogram(downHighpass,Fs)
-% ylim([0 8000])
-% subplot(2,1,2);
-% makeSpectrogram(downLowpass,Fs)
-% ylim([0 8000])
+downHighpassTop = downsample(highpassTopAudio,2);
+downHighpassBottom = downsample(highpassBottomAudio,2);
+downLowpassTop = downsample(lowpassTopAudio,2);
+downLowpassBottom = downsample(lowpassBottomAudio,2);
+Fs = Fs/2
 
-%audiowrite('teamG5-highpassdown2.wav',downHighpass,Fs);
-%audiowrite('teamG5-lowpassdown2.wav',downLowpass,Fs);
+figure
+subplot(2,1,1);
+makeSpectrogram(downHighpassTop,Fs)
+ylim([0 Fs/2])
+subplot(2,1,2);
+makeSpectrogram(downHighpassBottom,Fs)
+ylim([0 Fs/2])
+figure
+subplot(2,1,1);
+makeSpectrogram(downLowpassTop,Fs)
+ylim([0 Fs/2])
+subplot(2,1,2);
+makeSpectrogram(downLowpassBottom,Fs)
+ylim([0 Fs/2])
+
+audiowrite('teamG5-highpassdowntop.wav',downHighpassTop,Fs+.5);
+audiowrite('teamG5-highpassdownbottom.wav',downHighpassBottom,Fs+.5);
+audiowrite('teamG5-lowpassdowntop.wav',downLowpassTop,Fs+.5);
+audiowrite('teamG5-lowpassdownbottom.wav',downLowpassBottom,Fs+.5);
 
 end
 
@@ -69,10 +127,10 @@ Fs = Fs/2
 
 subplot(2,1,1);
 makeSpectrogram(downHighpass,Fs)
-ylim([0 8000])
+ylim([0 Fs/2])
 subplot(2,1,2);
 makeSpectrogram(downLowpass,Fs)
-ylim([0 8000])
+ylim([0 Fs/2])
 
 audiowrite('teamG5-highpassdown1.wav',downHighpass,Fs);
 audiowrite('teamG5-lowpassdown1.wav',downLowpass,Fs);
@@ -84,7 +142,7 @@ function splitSignal
 [audioFile,Fs] = audioread('teamG5-filteredspeech.wav');
 
 figure
-F = [0 7999/Fs 8000/Fs 1];
+F = [0 9999/Fs 10000/Fs 1];
 A = [1 1 0 0];
 [fil1, fil2] = firls(255,F,A);
 lowpassAudio = filter(fil1,fil2,audioFile);
@@ -98,10 +156,10 @@ highpassAudio = filter(fil3,fil4,audioFile);
 
 subplot(2,1,1);
 makeSpectrogram(highpassAudio,Fs);
-ylim([0 8000])
+ylim([0 Fs/2])
 subplot(2,1,2);
 makeSpectrogram(lowpassAudio,Fs);
-ylim([0 8000])
+ylim([0 Fs/2])
 
 audiowrite('teamG5-highpass.wav',highpassAudio,Fs);
 audiowrite('teamG5-lowpass.wav',lowpassAudio,Fs);
@@ -115,7 +173,7 @@ makeSpectrogram(audioFile,Fs)
 figure
 
 
-F = [0 15999/Fs 16000/Fs 1];
+F = [0 21999/Fs 22000/Fs 1];
 A = [1 1 0 0];
 [fil1, fil2] = firls(255,F,A);
 filteredAudio = filter(fil1,fil2,audioFile);
